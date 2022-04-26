acdf4a90f46da79d9d35f54183315a1e1c739c5f
Vitaliy1010 <vitalivitali00998877@gmail.com>
2022-04-26 21:04:46 +0300

    Added **, *, /

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ while activate:
         num2 = float(input("Num2: "))
         result = num1 - num2
         print(f"{num1} - {num2} = {result}")
+    elif user == "**":
+        num1 = float(input("Num1: "))
+        num2 = float(input("Num2: "))
+        result = num1 ** num2
+        print(f"{num1} ** {num2} = {result}")
+    elif user == "*":
+        num1 = float(input("Num1: "))
+        num2 = float(input("Num2: "))
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif user == "/":
+        num1 = float(input("Num1: "))
+        num2 = float(input("Num2: "))
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
     elif user == "exit" or user == "0":
         activate = False
         print("OFF")
